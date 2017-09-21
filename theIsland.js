307d21854938b60b9412e37861d14d2eb5ab5448
Nicolas Muntwyler <nicolas.muntwyler@gmail.com>
2017-09-21 20:34:47 +0200

    small cleanup

--- theIsland.js
+++ theIsland.js
@@ -1,30 +1,3 @@
-/*
-let arr = [];
-
-for (i = 0; i < 250; i = i + 8) {
-	arr.push(i + 1);
-}
-
-let newArray = arr.filter((item) => {
-	return item % 5 !== 0;
-})
-
-let newArray2 = newArray.filter((item) => {
-	return item % 7 !== 0;
-})
-
-let newArray3 = newArray2.filter((item) => {
-	return ((item % 7) - 2) === 0;
-})
-
-let newArray4 = newArray3.filter((item) => {
-	return ((item % 5) - 3) === 0;
-})
-
-
-console.log(newArray4);
-*/
-
 let coconuts;
 
 for (i = 1; i < 250; i = i + 8) {
@@ -35,4 +8,4 @@ for (i = 1; i < 250; i = i + 8) {
 
 const forEachPirate = (coconuts - 3) / 5;
 
-console.log(forEachPirate);
+console.log('For each Pirate', forEachPirate);
